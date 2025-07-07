bdde78cd960c658f52908b748c1e4e28c9cdebb7
BrunoBaumgartner78 <bruno@brainbloom.ch>
2025-07-07 19:51:26 +0200

    sanity

--- src/sanity/lib/client.js
+++ src/sanity/lib/client.js
@@ -1,10 +1,8 @@
-import { createClient } from 'next-sanity'
-
-import { apiVersion, dataset, projectId } from '../env'
+import { createClient } from '@sanity/client'
 
 export const client = createClient({
- projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
-  apiVersion,
-  useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
+  apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2023-01-01',
+  useCdn: true, // true = schneller, kein Draft Content; false = aktuellerer Content (langsamer)
 })
