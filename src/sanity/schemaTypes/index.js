ac2d0d0f052d928cf78a98af813b776b6d81c5d8
BrunoBaumgartner78 <bruno@brainbloom.ch>
2025-07-10 00:13:06 +0200

    sanity index.js

--- src/sanity/schemaTypes/index.js
+++ src/sanity/schemaTypes/index.js
@@ -1,7 +1,7 @@
-import blockContentType from './blockContentType';
-import categoryType from './categoryType';
-import postType from './postType';
-import authorType from './authorType';
+import { blockContentType } from './blockContentType';
+import { categoryType } from './categoryType';
+import { postType } from './postType';
+import { authorType } from './authorType';
 
 export const schemaTypes = [
   blockContentType,
--- src/sanity/shemaTypes.js
+++ src/sanity/shemaTypes.js
@@ -1,24 +1,11 @@
-import { defineType, defineField } from 'sanity'
+import { blockContentType } from './blockContentType';
+import { categoryType } from './categoryType';
+import { postType } from './postType';
+import { authorType } from './authorType';
 
-export const schema = {
-  name: 'default',
-  types: [
-    defineType({
-      name: 'post',
-      title: 'Post',
-      type: 'document',
-      fields: [
-        defineField({
-          name: 'title',
-          title: 'Title',
-          type: 'string',
-        }),
-        defineField({
-          name: 'content',
-          title: 'Content',
-          type: 'text',
-        }),
-      ],
-    }),
-  ],
-}
+export const schemaTypes = [
+  blockContentType,
+  categoryType,
+  postType,
+  authorType,
+];
