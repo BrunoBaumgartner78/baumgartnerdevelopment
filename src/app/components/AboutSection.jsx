69849ff1240130284411b8be4e05aa528d337934
BrunoBaumgartner78 <bruno@brainbloom.ch>
2025-07-21 22:59:17 +0200

    about section optimiert

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -3,19 +3,16 @@ import styles from '../styles/AboutSection.module.css';
 
 export default function AboutSection() {
   return (
-    <section
-      className="about-text"
-      aria-labelledby="about-title"
-      role="region"
-    >
-      <h2 id="about-us-title" className={styles.title}>Über uns</h2>
-      
-      <p>
-        Wir sind ein innovatives Webdesign-Studio aus der Schweiz,
-        spezialisiert auf futuristische Interfaces, 3D-Webtechnologien und
-        interaktive Markenkommunikation. Unser Ziel ist es, einzigartige digitale
-        Erlebnisse zu schaffen, die beeindrucken und bleiben.
-      </p>
+   <section className="about-text" aria-labelledby="about-us-title">
+  <h2 id="about-us-title" className={styles.title}>Über uns</h2>
+  <p>
+    Wir sind ein innovatives Webdesign-Studio aus der Schweiz,
+    spezialisiert auf futuristische Interfaces, 3D-Webtechnologien und
+    interaktive Markenkommunikation. Unser Ziel ist es, einzigartige digitale
+    Erlebnisse zu schaffen, die beeindrucken und bleiben.
+  </p>
+
+
 
       <a
         href="/about-team"
